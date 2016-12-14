315b31babf0b22f8322d47b7f219e5a5ceec997c
Varun Babu Pozhath <varunbabu008@gmail.com>
2016-12-14 17:40:02 +0530

    Add todos reducer and tests

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -14,9 +14,9 @@ describe('Reducers',()=>{
       var res = reducers.searchTextReducer(df(''),df(action));
 
       expect(res).toEqual(action.searchText);
-    });
-
-    describe('showCompletedReducer',()=>{
+    });    
+  });
+  describe('showCompletedReducer',()=>{
       it('should toggle showCompleted',() =>{
         var action = {
           type: 'TOGGLE_SHOW_COMPLETED'
@@ -25,5 +25,46 @@ describe('Reducers',()=>{
         expect(res).toEqual(true);
       });
     });
+  describe('todosReducer',()=>{
+    it('should add new todo',()=>{
+      var action = {
+        type: 'ADD_TODO',
+        text:'walk the dog'
+      };
+      var res = reducers.todosReducer(df([]),df(action));
+      expect(res.length).toEqual(1);
+      expect(res[0].text).toEqual(action.text);
+
+    });
+    it('should toggle todo',()=>{
+      var todos = [{
+        id:'123',
+        text: 'Something',
+        completed: true,
+        createdAt: 123,
+        completedAt: 125
+      }];
+      var action = {
+        type: 'TOGGLE_TODO',
+        id:'123'
+      };
+      var res = reducers.todosReducer(df(todos),df(action));
+      expect(res[0].completed).toEqual(false);
+      expect(res[0].completedAt).toEqual(undefined);
+    });
   });
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
