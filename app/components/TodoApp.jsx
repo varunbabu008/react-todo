802fc6d6b3307d7a8421a5323bebf6ab494b4d71
Varun Babu Pozhath <varunbabu008@gmail.com>
2016-12-15 17:04:05 +0530

    refactor todosearch to work with redux store

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -6,8 +6,8 @@ var moment = require('moment');
 
 import TodoList from 'TodoList';
 import AddTodo from 'AddTodo';
-
-var TodoSearch = require('TodoSearch');
+import TodoSearch from 'TodoSearch';
+//var TodoSearch = require('TodoSearch');
 
 var TodoAPI = require('TodoAPI');
 
