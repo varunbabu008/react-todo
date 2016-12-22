1eef4d3d078cbacf6c8a1a1aa76201cb3e17bab2
Varun Babu Pozhath <varunbabu008@gmail.com>
2016-12-22 18:13:32 +0530

    modify problem

--- webpack.config.js
+++ webpack.config.js
@@ -62,7 +62,7 @@ module.exports =  {
     path.resolve(__dirname,'./node_modules/foundation-sites/scss')
     ]
   },
-  devtool: process.env.NODE_ENV === 'production' ? undefined : 'inline-source-map'
+  devtool: process.env.NODE_ENV === 'production' ? undefined : 'cheap-module-eval-source-map'
 };
 
 
