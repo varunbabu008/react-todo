076f757a008dfa4517f85a7418259d496bface91
Varun Babu Pozhath <varunbabu008@gmail.com>
2016-12-22 18:25:08 +0530

    prob1

--- webpack.config.js
+++ webpack.config.js
@@ -1,69 +1,64 @@
 var webpack = require('webpack');
 var path = require('path');
 
-process.env.NODE_ENV = process.env.NODE_ENV || 'development'
-module.exports =  {
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+module.exports = {
   entry: [
-  'script!jquery/dist/jquery.min.js',
-  'script!foundation-sites/dist/foundation.min.js',
-  './app/app.jsx'
+    'script!jquery/dist/jquery.min.js',
+    'script!foundation-sites/dist/foundation.min.js',
+    './app/app.jsx'
   ],
-  externals:{
+  externals: {
     jquery: 'jQuery'
   },
-  plugins:[
-  new webpack.ProvidePlugin({
-    '$': 'jquery',
-    'jQuery': 'jquery'
-  }),
-  new webpack.optimize.UglifyJsPlugin({
-    compressor: {
-      warnings: false
-    }
-  })
+  plugins: [
+    new webpack.ProvidePlugin({
+      '$': 'jquery',
+      'jQuery': 'jquery'
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        warnings: false
+      }
+    })
   ],
-
   output: {
     path: __dirname,
     filename: './public/bundle.js'
   },
-  resolve:{
+  resolve: {
     root: __dirname,
     modulesDirectories: [
-    'node_modules',
-    './app/components',
-    './app/api'
+      'node_modules',
+      './app/components',
+      './app/api'
     ],
     alias: {
-        app: 'app',
-        Main: 'app/components/Main.jsx',
-        applicationStyles:'app/styles/app.scss',
-        actions:'app/actions/actions.jsx',
-        reducers:'app/reducers/reducers.jsx',
-        configureStore:'app/store/configureStore.jsx'
-
-      },
-    extensions: ['','.js',['.jsx']]
+      app: 'app',
+      applicationStyles: 'app/styles/app.scss',
+      actions: 'app/actions/actions.jsx',
+      reducers: 'app/reducers/reducers.jsx',
+      configureStore: 'app/store/configureStore.jsx'
+    },
+    extensions: ['', '.js', '.jsx']
   },
-  module:{
+  module: {
     loaders: [
-    {
-      loader: 'babel-loader',
-      query: {
-        presets: ['react','es2015','stage-0']
-      },
-      test: /\.jsx?$/,
-      exclude: /(node_modules|bower_components)/
-    }
-      ]
+      {
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015', 'stage-0']
+        },
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/
+      }
+    ]
   },
-  sassLoader:{
-    includePaths:[
-    path.resolve(__dirname,'./node_modules/foundation-sites/scss')
+  sassLoader: {
+    includePaths: [
+      path.resolve(__dirname, './node_modules/foundation-sites/scss')
     ]
   },
   devtool: process.env.NODE_ENV === 'production' ? undefined : 'cheap-module-eval-source-map'
 };
-
-
-
